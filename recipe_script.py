71b3c520cb07b8fde1fe46788584c9871cd50a5f
Alex Clark <aclark@aclark.net>
2011-07-26 11:10:09 -0400

    Add files

---
+++ recipe_script.py
@@ -0,0 +1,3 @@
+# Can something be both a zc.buildout recipe and a stand-alone script?
+
+
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='recipe_script',
+    py_modules=['recipe_script']
+    )
