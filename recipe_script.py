baf6d3b056a401e63a7ebf5b7e311d8cf0818b8d
Alex Clark <aclark@aclark.net>
2011-07-26 14:05:26 -0400

    Wrap it up

--- recipe_script.py
+++ recipe_script.py
@@ -1,6 +1,13 @@
 # Can something be both a zc.buildout recipe and a stand-alone script?
 
-
-
 def script():
-    pass
+    print 'I am a console script!'
+
+class Recipe(object):
+    def __init__(self, buildout, name, options):
+        pass
+    def install(self):
+        print 'I am a recipe!'
+        return ()
+    def update(self):
+        pass
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='recipe_script',
+    entry_points={
+#        'console_scripts': 'script = recipe_script:script',
+        'zc.buildout': 'default = recipe_script:Recipe',
+    },
     py_modules=[
         'recipe_script',
     ],
-    entry_points={
-        'console_scripts': 'script = recipe_script:script',
-    }
 )
