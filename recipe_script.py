99d8bb2443259640c02e996ca820c986f005d5bb
Alex Clark <aclark@aclark.net>
2011-07-26 14:14:33 -0400

    Wording

--- recipe_script.py
+++ recipe_script.py
@@ -1,4 +1,4 @@
-# Question: Can a package be both a zc.buildout recipe and a stand-alone script?
+# Question: Can a package contain both a zc.buildout recipe and a setuptools console script?
 # Answer: Yes!
 
 def script():
