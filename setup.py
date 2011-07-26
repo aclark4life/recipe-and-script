2ae7da3df9e4be54356196c13f8edfd35e723024
Alex Clark <aclark@aclark.net>
2011-07-26 14:17:13 -0400

    Put back the console script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='recipe_script',
     entry_points={
-#        'console_scripts': 'script = recipe_script:script',
+        'console_scripts': 'script = recipe_script:script',
         'zc.buildout': 'default = recipe_script:Recipe',
     },
     py_modules=[
